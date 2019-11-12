fbbbbad28592107e148c830eb86f9e1cd7a102d9
Hayley <hb@blkboxmedia.com>
2019-11-12 12:08:29 +0000

    Finish prototypes

--- JavaScript/Work/Pluralsight/Intermediate/JavaScript-Objects,Prototypes and Classes/3-JavaScript Prototypes and Interfaces/2-Instance vs Prototype/demo.js
+++ JavaScript/Work/Pluralsight/Intermediate/JavaScript-Objects,Prototypes and Classes/3-JavaScript Prototypes and Interfaces/2-Instance vs Prototype/demo.js
@@ -7,58 +7,17 @@
  let person = {firstname:'Jim'}; 
  display(person.__proto__);*/
 /*Person constructor function*/
-function Person(firstName, lastName,age){
-	this.firstName = firstName;
+function Person(firstName, lastName){
+	this.firstname = firstName;
 	this.lastName = lastName;
-	this.age = age;
-
-	Object.defineProperty(this, 'fullName',{
-		get: function(){
-			return this.firstName + ' ' + this.lastName	
-		},
-		enumerable: true
-	});
 }
 
-function Student(firstName, lastName, age)
-{
-	/*Allos us to call the person function while setting the context of this*/
-	Person.call(this, firstName, lastName, age);
-	this._enrolledCourses = [];
-
-	this.enroll = function(courseId){
-		this._enrolledCourses.push(courseId);
-	};
-
-	this.getCourses = function(){
-		return this.fullName + "'s enrolled courses are: " + this._enrolledCourses.join(', ');
-	};
-
-}
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.constructor = Student;
-
-let jim = new Student('Jim','Cooper', 29);
-jim.enroll('CS205');
-jim.enroll('MA101');
-jim.enroll('PS101');
-
-display(jim);
-display(jim.__proto__);
-display(jim.__proto__.__proto__);
-display(jim.__proto__.__proto__.__proto__);
-
-
-display(jim.getCourses());
-
-
-/*Person.prototype.age = 29;
+Person.prototype.age = 29;
 
 display(Person.prototype);
 
 let jim = new Person('Jim','Cooper');
-let sofia = new Person('sofia','Cooper');*/
+let sofia = new Person('sofia','Cooper');
 
 /*sofia.__proto__.age = 19;
 
