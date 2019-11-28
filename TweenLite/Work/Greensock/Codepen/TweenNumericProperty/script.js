db1bbb0a17eac6090266d8d19cf5429bc3d492c8
Hayley <hb@hungrybeargaming.co.uk>
2019-11-28 13:54:47 +0000

    Tween Numeric Property

---
+++ TweenLite/Work/Greensock/Codepen/TweenNumericProperty/script.js
@@ -0,0 +1,10 @@
+var demo = {score:0};
+var	scoreDisplay = document.getElementById("scoreDisplay");
+
+//create a tween that changes the value of the score property of the demo object from 0 to 100 over the course of 20 seconds
+							
+var tween = TweenLite.to(demo, 20, {score: 100, onUpdate:showScore})
+
+function showScore(){
+	//scoreDisplay.innerHTML
+}
