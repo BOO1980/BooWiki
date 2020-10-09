36b79026de20e3ef0cf60ce93f1707bd4ad8c83c
Hayley <hb@hungrybeargaming.co.uk>
2020-10-09 10:13:16 +0100

    OOO javaScript

---
+++ JavaScript/Projects/CarvedRockFitness/home.js
@@ -0,0 +1,4 @@
+/*
+alert("Hello World");
+alert("Second Message");*/
+showMessage("Title...");
--- JavaScript/Work/Books/HeadFirstJavaScript/Chapter2/1-battleship/battleship.js
+++
@@ -1,8 +0,0 @@
-var location1;
-var location2;
-var location3;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
-
