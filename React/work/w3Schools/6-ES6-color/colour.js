b39b5742cb1252a52765f716da9c3e424faabeaf
Hayley <hb@hungrybeargaming.co.uk>
2020-11-09 19:41:22 +0000

    CU-9wt1b9 - React ES6 - this

--- React/work/w3Schools/6-ES6-color/colour.js
+++ React/work/w3Schools/6-ES6-color/colour.js
@@ -2,13 +2,15 @@ class Header{
     constructor() {
         this.color = "Red";
     }
-}
-
-//Arrow function
-changeColor = () =>{
-    document.getElementById("demo").innerHTML += this;
+    //Arrow function
+    changeColor = function(){
+        document.getElementById("demo").innerHTML += this;
+    }
 }
 myheader = new Header();
 
 //this window object calls the function
-window.addEventListener("load",)
+    window.addEventListener("load",myheader.changeColor);
+    document.getElementById("btn").addEventListener("click",myheader.changeColor);
+
+
