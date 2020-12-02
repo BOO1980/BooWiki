77bb57bbb110eda038e2dad8732f806ed25569f4
Hayley <hb@hungrybeargaming.co.uk>
2020-12-02 12:45:39 +0000

    CU-9yxzr8 - State[1]

--- React/work/w3Schools/12-state-car2/src/index.js
+++ React/work/w3Schools/12-state-car2/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Car extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            brand: "Ford",
+            model: "Mustang",
+            color: "Red",
+            year: 1964
+        };
+    }
+    render(){
+        return(
+            <div>
+                <h1>My {this.state.brand}</h1>
+                <p>
+                    It is a {this.state.color}
+                    {this.state.model}
+                    from {this.state.year}
+                </p>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Car />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
